4a1487b476d6c0fbd5084d94c137ab272b7b4b6c
Caique.Braga <caiquebsousa@outlook.com>
2024-11-27 05:11:20 -0300

    [REFS: 0004 - Feat - Inicio da criação da service de produto]

---
+++ src/main/java/com/crud/service/ProdutoService.java
@@ -0,0 +1,11 @@
+package main.java.com.crud.service;
+
+import main.java.com.crud.model.Produto;
+import main.java.com.crud.repository.ProdutoRepository;
+
+import java.util.List;
+
+public class ProdutoService {
+
+    private final ProdutoRepository produtoRepository = new ProdutoRepository();
+}
